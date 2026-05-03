4a25844b86d167ca5cc5aa6f8db9715b4fe5d086
Amna Mumtaz <amna.mumtaz02@gmail.com>
2026-05-03 10:28:11 +0500

    externalize only bcryptjs and jose

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['bcryptjs', 'jose', '@auth/core']
+    serverComponentsExternalPackages: ['bcryptjs', 'jose']
   }
 }
 
