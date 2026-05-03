f0cb75d2abae5129ee2929e7a9bbcba6ce4fb269
Amna Mumtaz <amna.mumtaz02@gmail.com>
2026-05-03 16:56:20 +0500

    Revert "updated next.config.mjs"
    This reverts commit be33883da3ccc50a07f1ba564f4a8311d84e4b9e.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['bcryptjs', 'jose', 'next-auth', '@auth/core']
+    serverComponentsExternalPackages: ['bcryptjs', 'jose']
   }
 };
 export default nextConfig;
