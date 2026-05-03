d1d16f0d1b5d34b988ddb5d2bdab32e9d6b3dd8e
Amna Mumtaz <amna.mumtaz02@gmail.com>
2026-05-03 10:35:15 +0500

    use node 22 and simple next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,6 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['bcryptjs']
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'jose': require.resolve('jose')
-      }
-    }
-    return config
   }
 }
 
